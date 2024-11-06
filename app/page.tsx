df18f167197faa00522aad188e63688575715754
olenadm <olenad@gmail.com>
2024-11-06 15:51:46 +0100

    Fixed margins

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,14 @@ export default function Home() {
         <Row><Col><h1 className='my-5'>Please choose demo site</h1></Col></Row>
         <Row className="gx-5 align-items-center justify-content-center">
           <Col sm={6}>
-            <Card bg='primary'>
+            <Card bg='primary' className='mb-3'>
               <CardBody>
                 <a href="/brandtv" className='btn text-white'>Brand TV Landing</a>
               </CardBody>
             </Card>
           </Col>
           <Col sm={6}>
-            <Card bg='danger'>
+            <Card bg='danger' className='mb-3'>
               <CardBody>
                 <a href="/replay" className='btn text-white'>
                   Replay
