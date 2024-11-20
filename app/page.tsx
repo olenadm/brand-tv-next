7ba0915483e05bf4546d03d07182530759c25a1b
Alex Walker <alex.walker@whiteops.com>
2024-11-20 15:46:42 +0000

    making homepage the brand tv page

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,33 @@
 /* eslint-disable @next/next/no-html-link-for-pages */
+import Hero from "./components/Hero";
 
 import { Container, Row, Col, Card, CardBody } from "react-bootstrap";
 
 export default function Home() {
   return (
-    <>
-      <Container className="my-auto px-5">
-        <Row><Col><h1 className='my-5'>Please choose demo site</h1></Col></Row>
-        <Row className="gx-5 align-items-center justify-content-center">
-          <Col sm={6}>
-            <Card bg='primary' className='mb-3'>
-              <CardBody>
-                <a href="/brandtv" className='btn text-white'>Brand TV Landing</a>
-              </CardBody>
-            </Card>
-          </Col>
-          <Col sm={6}>
-            <Card bg='danger' className='mb-3'>
-              <CardBody>
-                <a href="/replay" className='btn text-white'>
-                  Replay
-                </a>
-              </CardBody>
-            </Card>
-          </Col>
-        </Row>
-      </Container>
-    </>
+    <Hero />
+    // <>
+    //   <Container className="my-auto px-5">
+    //     <Row><Col><h1 className='my-5'>Please choose demo site</h1></Col></Row>
+    //     <Row className="gx-5 align-items-center justify-content-center">
+    //       <Col sm={6}>
+    //         <Card bg='primary' className='mb-3'>
+    //           <CardBody>
+    //             <a href="/brandtv" className='btn text-white'>Brand TV Landing</a>
+    //           </CardBody>
+    //         </Card>
+    //       </Col>
+    //       <Col sm={6}>
+    //         <Card bg='danger' className='mb-3'>
+    //           <CardBody>
+    //             <a href="/replay" className='btn text-white'>
+    //               Replay
+    //             </a>
+    //           </CardBody>
+    //         </Card>
+    //       </Col>
+    //     </Row>
+    //   </Container>
+    // </>
   );
 }
