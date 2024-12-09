bd78c1f598f96530359f997fa2590b006944d25d
olenadm <olenad@gmail.com>
2024-12-09 18:37:24 +0100

    New video links

--- app/api/categories/data.ts
+++ app/api/categories/data.ts
@@ -34,8 +34,8 @@ export const categories = [
 export const videos = [
   {
     id: '1',
-    name: "Six 1 of 1s at The National",
-    url: "https://player.vimeo.com/video/1030044032?h=90b270b5af&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+    name: "Replay Gives Back",
+    url: "https://player.vimeo.com/video/1033764326?h=83dd8cf38c&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
     cat: ["professional", "company"],
     anotherCat: 'featured',
     metadata: {
@@ -48,8 +48,8 @@ export const videos = [
   },
   {
     id: '2',
-    name: "Brandon Miller visits Replay Sports Cards",
-    url: "https://player.vimeo.com/video/1030047028?h=cd60b77c53&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+    name: "Replay Sports Cards Welcome Video",
+    url: "https://player.vimeo.com/video/1037354669?h=353c6329e3&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
     cat: ["prom"],
     anotherCat: 'youlike',
     metadata: {
@@ -62,8 +62,8 @@ export const videos = [
   },
   {
     id: '3',
-    name: "Spelling R-E-P-L-A-Y at Toronto Card Show",
-    url: "https://player.vimeo.com/video/1030044969?h=0467a44cda&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+    name: "Puka Nacuna Favorite Sports Cards by CodyE",
+    url: "https://player.vimeo.com/video/1037354702?h=67deb56280&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
     cat: ["company"],
     anotherCat: 'recommended',
     metadata: {
@@ -77,10 +77,24 @@ export const videos = [
 
   {
     id: '4',
-    name: "Grant Williams Rips His First Football Box",
-    url: "https://player.vimeo.com/video/1030043780?h=fd7953883d&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+    name: "Tom Brady Favorite Sports Cards BY CodyE",
+    url: "https://player.vimeo.com/video/1037354687?h=30739a507a&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
     cat: ["films"],
     anotherCat: 'recommended',
+    metadata: {
+      actors: [""],
+      release_date: "24/02/1980",
+      upload_date: "...",
+      length: "...",
+      director: "Fellini Frederico"
+  }
+  },
+  {
+    id: '5',
+    name: "Patrick Mahommes Favorite _ Card (byJamieE)",
+    url: "https://player.vimeo.com/video/1037354715?h=47eae0ba49&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479",
+    cat: ["films"],
+    anotherCat: ['recommended', 'youlike'],
     metadata: {
       actors: [""],
       release_date: "24/02/1980",
